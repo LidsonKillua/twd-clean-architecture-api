5d0d86ee0e672f7fabf3648f38a500ba2d18b5cd
lid <lidson@oriontec.com.br>
2024-06-13 22:20:06 -0300

    feat: user data interface

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from "./user-data"
+
+describe('registrar usuário na lista de emails use case', () => {
+  test('should add user with complete data to mailing list', () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+    // const response = await usecase.RegisterUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail(email)
+    // expect((await user).name).toBe(name)
+  })
+})
