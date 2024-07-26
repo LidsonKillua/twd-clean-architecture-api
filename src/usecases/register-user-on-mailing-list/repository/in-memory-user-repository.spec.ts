66807dd88fccc0b7bfa325a550e207d61568257b
lid <lidson@oriontec.com.br>
2024-07-26 16:07:00 -0300

    test: adicionar e buscar o usuário

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -5,7 +5,18 @@ describe('In memory User repository', () => {
   test('should return null if user is not found', async () => {
     const users: UserData[] = []
     const userRepo = new InMemoryUserRepository(users)
-    const user = await userRepo.findUSerByEmail('any@mail.com')
+    const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user).toBeNull()   
-  })    
+  })
+  
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@anymail.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({name, email})
+    const user = await userRepo.findUserByEmail(email)
+    expect(user.name).toBe(name)
+  })
 })
+
