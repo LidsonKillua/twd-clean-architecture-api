03b0229f6226c640bb5127984145b8993380c4e8
lid <lidson@oriontec.com.br>
2024-07-31 18:32:28 -0300

    test: criar um usuário válido

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -12,14 +12,22 @@ describe('User domain entity', () => {
 
     test('should not create user with invalid name(too few characters)', () => {
         const name = 'O           '
-        const error = User.create({ name: name, email: 'teste@gmail.com'})
+        const error = User.create({ name: name, email: 'teste@gmail.com' })
         expect(error).toEqual(left(new InvalidNameError()))
     })
 
     test('should not create user with invalid name(too many characters)', () => {
         const name = 'O'.repeat(257)
-        const error = User.create({ name: name, email: 'teste@gmail.com'})
+        const error = User.create({ name: name, email: 'teste@gmail.com' })
         expect(error).toEqual(left(new InvalidNameError()))
+    })
+
+    test('should create user with valid name and email', () => {
+        const validName = 'Lidson Oliveira'
+        const validEmail = 'lidson@oriontec.com.br'
+        const user: User = User.create({ name: validName, email: validEmail }).value as User
+        expect(user.name.value).toEqual(validName)
+        expect(user.email.value).toEqual(validEmail)
     })
 })
 
