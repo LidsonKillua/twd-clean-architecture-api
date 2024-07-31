87681da1825df0025aa33f7bd36adb804a10fe08
lid <lidson@oriontec.com.br>
2024-07-31 18:04:18 -0300

    test: verificar tamanho do nome para criar

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,5 +1,6 @@
 import { left } from "../shared/either"
 import { InvalidEmailError } from "./errors/invalid-email-error"
+import { InvalidNameError } from "./errors/invalid-name-error"
 import { User } from "./user"
 
 describe('User domain entity', () => {
@@ -8,5 +9,17 @@ describe('User domain entity', () => {
         const error = User.create({ name: 'name', email: invalidEmail})
         expect(error).toEqual(left(new InvalidEmailError()))
     }) 
+
+    test('should not create user with invalid name(too few characters)', () => {
+        const name = 'O           '
+        const error = User.create({ name: name, email: 'teste@gmail.com'})
+        expect(error).toEqual(left(new InvalidNameError()))
+    })
+
+    test('should not create user with invalid name(too many characters)', () => {
+        const name = 'O'.repeat(257)
+        const error = User.create({ name: name, email: 'teste@gmail.com'})
+        expect(error).toEqual(left(new InvalidNameError()))
+    })
 })
 
