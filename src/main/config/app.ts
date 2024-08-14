82ea7080ef03dc63bb6a9fa154c36ef1c0130744
lid <lidson@oriontec.com.br>
2024-08-14 17:36:21 -0300

    feat: config register route

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
-import SetupMiddleware from '@/main/config/middleware'
+import setupMiddleware from '@/main/config/middleware'
+import setupRoutes from '@/main/config/routes'
 
 const app = express()
-SetupMiddleware(app)
+setupMiddleware(app)
+setupRoutes(app)
 
 export default app
