f05014cf51fbc341b137b7cee3feb5f94ea2dcd5
lid <lidson@oriontec.com.br>
2024-10-07 19:29:01 -0300

    test: find all users

--- test/external/repositories/mongodb/mongodb-user-repository.spec.ts
+++ test/external/repositories/mongodb/mongodb-user-repository.spec.ts
@@ -24,4 +24,22 @@ describe('MongoDB User Repository', () => {
         await sut.add(user)
         expect(await sut.exists(user)).toBeTruthy()
     })    
+
+    test('find all users in collection', async () => {
+        const sut = new MongodbUserRepository()
+        const user = {
+            name: 'any_name',
+            email: 'any@mail.com'  
+        }
+        const user2 = {
+            name: 'second_name',
+            email: 'any2@mail.com'
+        }
+        await sut.add(user)
+        await sut.add(user2)
+        const users = await sut.findAllUsers()
+        expect(users).toHaveLength(2)
+        expect(users[0].name).toBe('any_name')
+        expect(users[1].email).toBe('any2@mail.com')
+    })
 })
