e83670ae0131dc2fcfad245f66f8dda4c7d00c01
lid <lidson@oriontec.com.br>
2024-10-08 09:47:29 -0300

    refactor: removendo if desnecessário

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -14,20 +14,20 @@ export class RegisterUserController {
   public async handle(request: HttpRequest): Promise<HttpResponse> {
     try {
       if (!(request.body.name) || !(request.body.email)) {
-        let missingParam = !request.body.name ? 'name ' : '';
-        missingParam += !request.body.email ? 'email' : '';
-        return badRequest(new MissingParamError(missingParam.trim()));
+        let missingParam = !request.body.name ? 'name ' : ''
+        missingParam += !request.body.email ? 'email' : ''
+        return badRequest(new MissingParamError(missingParam.trim()))
       }
 
-      const userData: UserData = request.body;
-      const response = await this.usecase.perform(userData);
+      const userData: UserData = request.body
+      const response = await this.usecase.perform(userData)
 
       if (response.isLeft()) {
-        return badRequest(response.value);
+        return badRequest(response.value)
       }
-      else if (response.isRight()) {
-        return created(response.value)
-      }
+      
+      return created(response.value)
+
     } catch (error) {
       return serverError(error);
     }
