6b38c7fe0288d7331cd49566c8f98f6a82a198f7
lid <lidson@oriontec.com.br>
2024-10-10 18:15:36 -0300

    feat: utilizar User no caso de uso

--- src/usecases/send-email/send-email.ts
+++ src/usecases/send-email/send-email.ts
@@ -1,25 +1,14 @@
-import { User, UserData } from "@/entities";
+import { User } from "@/entities";
 import { UseCase } from "@/usecases/ports";
 import { EmailOptions, EmailService } from "@/usecases/send-email/ports";
-import { Either, left } from "@/shared";
+import { Either } from "@/shared";
 import { MailServiceError } from "@/usecases/errors";
-import { InvalidEmailError, InvalidNameError } from "@/entities/errors";
 
 export class SendEmail implements UseCase {
     constructor(private readonly emailOptions: EmailOptions, private readonly emailService: EmailService) {}
 
-    async perform(request: UserData): 
-        Promise<Either<InvalidNameError | InvalidEmailError | MailServiceError, EmailOptions>> {
-        
-        const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
-        
-        if (userOrError.isLeft()) {
-            return left(userOrError.value)
-        }
-
-        const user = userOrError.value;
-
-        const greetings = `E aí ${user.name}, beleza?`;
+    async perform(user: User): Promise<Either<MailServiceError, EmailOptions>> {
+        const greetings = `E aí ${user.name.value}, beleza?`;
         const customizedHtml = greetings + '<br> <br>' + this.emailOptions.html;
         const mailInfo: EmailOptions = {
             host: this.emailOptions.host,
@@ -27,7 +16,7 @@ export class SendEmail implements UseCase {
             username: this.emailOptions.username,
             password: this.emailOptions.password,
             from: this.emailOptions.from,
-            to: `${user.name} <${user.email}>`,
+            to: `${user.name.value} <${user.email.value}>`,
             subject: this.emailOptions.subject,
             text: this.emailOptions.text,
             html: customizedHtml,
