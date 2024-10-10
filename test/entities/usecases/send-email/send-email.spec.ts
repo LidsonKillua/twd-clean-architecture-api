8e3be304a52991e87d9ce684f4513a8383e68466
lid <lidson@oriontec.com.br>
2024-10-10 11:15:27 -0300

    test: send email

---
+++ test/entities/usecases/send-email/send-email.spec.ts
@@ -0,0 +1,44 @@
+import { Either, right, Right} from "@/shared"
+import { MailServiceError } from "@/usecases/errors/mail-service-error"
+import { EmailOptions, EmailService } from "@/usecases/send-email/ports"
+import { SendEmail } from "@/usecases/send-email"
+
+const fromName = 'Test'
+const fromEmail = 'mail@mail.com'
+const toName = 'Test'
+const toEmail = 'anymail@mail.com'
+const subject = 'Teste subject'
+const emailBody = 'Teste body'
+const emailBodyHtml = '<p>Teste body</p>'
+const attachment = [{
+    filename: '../resources/text.txt',
+    contentType: 'text/plain',
+}]
+
+const mailOptions: EmailOptions = {
+    host: 'teste',
+    port: 2525,
+    username: 'username',
+    password: 'password',
+    from: `${fromName} <${fromEmail}>`,
+    to: `${toName} <${toEmail}>`,
+    subject: subject,
+    text: emailBody,
+    html: emailBodyHtml,
+    attachments: attachment,
+}
+
+class MailServiceStub implements EmailService {
+    async send(emailOptions: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
+        return right(emailOptions)
+    }
+}
+
+describe('Send email use case', () => {
+    test('Should send email', async () => {
+        const mailServiceStub = new MailServiceStub()
+        const sut = new SendEmail(mailOptions, mailServiceStub)
+        const response = await sut.perform({ name: toName, email: toEmail })
+        expect(response).toBeInstanceOf(Right)
+    })
+})
