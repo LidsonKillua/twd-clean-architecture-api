cd8bd6bae8be109c21814853e6d9047ce40a117c
lid <lidson@oriontec.com.br>
2024-10-10 17:59:40 -0300

    feat: utilizar User no UseCase

--- test/entities/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ test/entities/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,4 +1,4 @@
-import { UserData } from "@/entities"
+import { User, UserData } from "@/entities"
 import { RegisterUserOnMailingList } from "@/usecases/register-user-on-mailing-list"
 import { UserRepository } from "@/usecases/register-user-on-mailing-list/ports"
 import { InMemoryUserRepository } from "@/usecases/register-user-on-mailing-list/repository"
@@ -11,9 +11,10 @@ describe('registrar usuário na lista de emails use case', () => {
   test('should add user with complete data to mailing list', async () => {
     const name = 'any_name'
     const email = 'any@email.com'
-    const response = await usecase.perform({ name, email })
-    const user = repo.findUserByEmail(email)
-    expect((await user).name).toBe('any_name')
-    expect(response.value.name).toBe('any_name')
+    const user = User.create({ name, email }).value as User
+    const response = await usecase.perform(user)
+    const addeduser = repo.findUserByEmail(email)
+    expect((await addeduser).name).toBe('any_name')
+    expect(response.name).toBe('any_name')
   })
 })
