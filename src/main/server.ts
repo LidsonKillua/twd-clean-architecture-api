f636bd68a85dca23c2dda816e9c17855017a0eee
lid <lidson@oriontec.com.br>
2024-10-11 14:42:28 -0300

    refactor: use port 5000

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,8 +4,8 @@ import { MongoHelper } from '@/external/repositories/mongodb/helper'
 MongoHelper.connect(process.env.MONGO_URL)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(process.env.PORT || 5000, () => {
-      console.log('Server running...')
+    app.listen(5000, () => {
+      console.log('Server running at http://localhost:5000')
     })
   })
   .catch(console.error)
